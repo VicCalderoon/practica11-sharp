fbd0af016c76fae0a84b0ddebf364f7dafad921a
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-29 23:07:57 -0700

    Agregar archivos de proyecto.

---
+++ practica11 sharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Schema;
+
+namespace TABLA_DE_MULTIPLICAR_CON_WHILE
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int NUM, I, RESUL;
+            string linea;
+            Console.Write("DIGITE NÚMERO: "); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            I = 1;
+            while ((I <= 12))
+            {
+                RESUL = NUM * I;
+                Console.WriteLine("{0} * {1} = {2} ", NUM, I, RESUL);
+                I = I + 1;
+            }
+            Console.Write("Pulse una tecla: "); Console.ReadLine();
+        }
+    }
+}
